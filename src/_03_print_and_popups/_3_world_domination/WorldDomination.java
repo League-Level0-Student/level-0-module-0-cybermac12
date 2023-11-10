f440932ce389a031076ca0e50da40e95ab117d01
chickennuggetduck33 <luke.fun59@gmail.com>
2023-11-09 16:59:05 -0800

    working on a game that is a maze i have done nothing yet

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,11 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String code = JOptionPane.showInputDialog("Do you know how to code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if (code.equals("yes") ) { JOptionPane.showMessageDialog(null,"You shall rule the world Ha Ha Ha ");}
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+		if (code.equals("no")) { JOptionPane.showMessageDialog(null,"Go back to bed ");}
 	}
 }
 
